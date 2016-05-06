67e0d9d546ed676a261efec3b62e03e849390417
lazylad91 <parteek.makkar@sjsu.edu>
2016-05-05 23:04:45 -0700

    Initial Project setup

---
+++ src/main/java/TheApp275Final/term/config/MvcConfiguration.java
@@ -0,0 +1,44 @@
+package TheApp275Final.term.config;
+
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan(basePackages="TheApp275Final.term")
+@Import({Hibernateconfig.class})
+@EnableWebMvc
+public class MvcConfiguration extends WebMvcConfigurerAdapter{
+
+	@Bean
+	public ViewResolver getViewResolver(){
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+	
+	 @Bean
+	    public PropertyPlaceholderConfigurer getPropertyPlaceholderConfigurer()
+	    {
+	        PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();
+	        ppc.setLocation(new ClassPathResource("application.properties"));
+	        ppc.setIgnoreUnresolvablePlaceholders(true);
+	        return ppc;
+	    }
+
+	
+}
